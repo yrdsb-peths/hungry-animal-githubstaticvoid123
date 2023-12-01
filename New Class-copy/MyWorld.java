1339e76aeade7dba42f6edd3dc52d6f01746bf89
githubstaticvoid123 <440019464@gapps.yrdsb.ca>
2023-12-01 09:04:07 -0500

    Update MyWorld.java

--- New Class-copy/MyWorld.java
+++ New Class-copy/MyWorld.java
@@ -19,7 +19,29 @@ public class MyWorld extends World
         super(600, 400, 1); 
         Animal elephant = new Animal(); 
         addObject(elephant, 300, 300); 
-        Apple apple = new Apple(); 
-        addObject(apple, 500, 200); 
+        spawnApple(); 
+        
+        //Create a Label 
+        scoreLabel = new Label("0", 20); 
+        addObject(scoreLabel, 50, 50); 
+    }
+    
+     public void increaseScore()
+    {
+        score++; 
+        scoreLabel.setLocation(0, 80); 
+    }
+    
+    public void gameOver() 
+    {
+        Label gameOverLabel = new Label("Game Over", 100); 
+        addObject(gameOverLabel, 300, 200); 
+    }
+    public void spawnApple()
+    {
+        int x = Greenfoot.getRandomNumber(600); 
+        int y = 0; 
+        Apple apple = new Apple (); 
+        addObject(apple, x, y); 
     }
 }
